57d650c7dae111d5f204e2d46e0334efed698732
“juandrzej” <programming.yippee729@passinbox.com>
2025-03-16 19:20:29 +0100

    initial commit

--- infinite_iterators.py
+++ infinite_iterators.py
@@ -9,8 +9,16 @@ def test_count() -> None:
     for _ in range(10):
         print((next(count1), next(count2), next(count3), next(count4)))
 
+def test_cycle() -> None:
+    people: list[str] = ['Tom', 'Bob', 'Alice']
+    my_cycle: cycle = cycle(people)
+    
+    for _ in range(5):
+        print(next(my_cycle))
+
 def main() -> None:
     test_count()
+    test_cycle()
 
 if __name__ == '__main__':
     main()
