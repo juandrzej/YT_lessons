b6723c49a4f075b8b332bbd4337d9b3666b50ad7
“juandrzej” <programming.yippee729@passinbox.com>
2025-03-22 13:23:01 +0100

    masking string

--- unspecified_discoveries.py
+++ unspecified_discoveries.py
@@ -10,9 +10,40 @@ def new_func() -> None:
     print('new_func() called!')
 
 
+def input_or_na() -> None:
+    user_input = input('Name: ')
+    name = user_input or 'N/A'
+    print(name)
+
+
+def mask_str(
+    current_str: str,
+    mask_character: str = "*",
+    start: int = 2,
+    end: int = 2
+) -> str:
+    
+    a = current_str[:start]
+    b = mask_character * len(current_str[start:end])
+    c = current_str[end:]
+
+    return a + b + c
+    
+    
+def masking_str() -> None:
+    current_str = "1234567890"
+    print(f"{current_str=}")
+
+    masked_str = mask_str(
+        current_str, "*", 0, -2
+    )
+    print(f"{masked_str=}")
+
 def main() -> None:
-    old_func()
-    new_func()
+    # old_func()
+    # new_func()
+    # input_or_na()
+    masking_str()
 
 if __name__ == '__main__':
     main()
